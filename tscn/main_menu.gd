1bd8dd762f554322aa9cabbaaa14302d77b0d961
Nathan Simpson <nasimpson@csumb.edu>
2024-04-05 18:41:21 -0700

    added quick main menu and tested export to itch.io successfully

---
+++ tscn/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://tscn/World_V2.tscn")
+
+
+func _on_option_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
